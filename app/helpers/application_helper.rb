584a7b48a6105dc5317bd0694fd0f664e8dca864
ankurbajad <ankurbajad786@gmail.com>
2017-04-05 20:54:36 +0530

    feature add links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def link_account
   	html = ""
   	unless user_signed_in?
-  	  html += link_to "My Account",new_user_registration_path,class: "phone-box  clear-box text-center"
+  	  html += link_to "My Account",new_user_session_path,class: "phone-box  clear-box text-center"
     else
     	# html += "<div class='btn-group show-on-hover'> <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'> Action <span class='caret'></span> </button> <ul class='dropdown-menu' role='menu'> <li><a href='#'>Action</a></li></ul> </div>"
 
