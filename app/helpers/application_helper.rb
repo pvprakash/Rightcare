ab3a1cf627e545fc464cff9199bd8cdc6b90ccde
ankurbajad <ankurbajad786@gmail.com>
2017-04-05 13:26:42 +0530

    feature add devise user & role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def link_account
+  	html = ""
+  	unless user_signed_in?
+  	  html += link_to "My Account",new_user_registration_path,class: "phone-box  clear-box text-center"
+    else
+    	# html += "<div class='btn-group show-on-hover'> <button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'> Action <span class='caret'></span> </button> <ul class='dropdown-menu' role='menu'> <li><a href='#'>Action</a></li></ul> </div>"
+
+    	html += "<div class='btn-group show-on-hover'>"
+    	html += "<button class='phone-box  clear-box text-center m-phonebox dropdown-toggle ' type='button' data-toggle='dropdown'>Hi #{current_user.first_name} <span class='fa fa-angle-down'><span></button>"
+      # html += "<button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'> Action <span class='caret'></span> </button>"
+  		html += "<ul class='dropdown-menu' role='menu'> <li>#{link_to 'Logout',destroy_user_session_path, method: :delete }</li></ul>"
+  		html += "</div>"
+     end
+    return html.html_safe
+  end
 end
