8ed15b0f23123158a6c13ce24ce2570c497c33a9
Ankur Bajad <ankurbajad786@gmail.com>
2017-04-19 17:26:54 +0530

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
- AdminUser.create!(email: 'admin1@example.com', password: 'password', password_confirmation: 'password')
+ AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
