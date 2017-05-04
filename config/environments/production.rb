6d744e4e331b620e29502786dfa17b16b9ca31dd
ankurbajad <ankurbajad786@gmail.com>
2017-05-04 11:10:16 +0530

    email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { :host => "ec2-54-245-63-90.us-west-2.compute.amazonaws.com" }
+config.action_mailer.default_url_options = { :host => "ec2-54-149-71-101.us-west-2.compute.amazonaws.com" }
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
