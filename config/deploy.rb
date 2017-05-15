14be0def644b018085518c0e3c91247bf9998c94
ankurbajad <ankurbajad786@gmail.com>
2017-05-15 16:30:14 +0530

    change in deploy rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,14 +32,16 @@ set :deploy_to, '/home/deploy/rightcare'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :delayed_job_bin_path, 'bin'
+
 namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
-        execute :touch, 'RAILS_ENV=production rake jobs:work &'
         execute :touch, 'sudo service nginx restart'
+        execute :touch, 'RAILS_ENV=production rake jobs:work &'
       # end
     end
   end
